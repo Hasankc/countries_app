a5378b01e7f92a8c471d67c3ae2a8c263c8ae6b2
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-02-09 09:27:03 +0200

    Add hook for all countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
 
-function App() {
-  return <div className='App'></div>
-}
 
-export default App
+  this.baseUrl = "https://restcountries.com/v3.1/";
+
+    async getAllCountries   () => {
+
+        const fetchData = await fetch(`${this.baseUrl}/all`)
+        .then(res) => {
+            if (res.ok) {
+                return res.jason();
+            } else {
+                throw new Error(res.error);
+            }
+            const resData = await this.getAllCountries.jason();
+            return resData;
+        } catch (error) {
+            return `Error all countries ${error}`;
+    }
+    
+    
+
--- src/custom-hooks/useCountries.js
+++ src/custom-hooks/useCountries.js
@@ -1,1 +1,34 @@
 // This hook is used to fetch all countries
+import  {useState, useEffect} from "react";
+
+const url = "https://restcountries.com/v3.1/all";
+
+const useCountries = function () {
+
+    const [data, setData] = useState();
+
+    const [error, setError] = useState();
+
+    const getData = async function () {
+        const resp = await fetch(url);
+        if(resp.ok) {
+            throw new Error("Response not ok!");
+
+        }
+
+        const data = await Response.jason();
+
+        setData(data);
+
+    } catch (err) {
+        setError(err.message);
+    }
+    useEffect(() => {
+        getData();
+    }, []);
+    return [data, error];
+
+}
+
+
+export default useCountries;
